c2f3aba8c3a61d1d5bfb31f9fb5961f3732af63b
Kushal Das <kushalstcet666@gmail.com>
2026-04-11 02:42:24 +0000

    datatypes learned

---
+++ Work/JavaScript/dataTypes.js
@@ -0,0 +1,28 @@
+"use strict";// tral all code as newer version
+
+//alert 3 + 3; // we are using node.js, not browser
+
+console.log(3 + 3); //code readability high
+console.log("Kushal");
+
+let name="Kushal" //String
+let age=18//number
+let isLoggedIn=true//boolean
+let state;
+state = null;
+
+// Primitive
+//  number => 2 to power 53
+//  binint 
+//  string => ""
+//  boolean => true/false
+//  null => standalone value
+//  undefined => value not assigned
+//  symbol => uniqueness
+
+//Object
+
+console.log(typeof null); //object
+console.log(typeof undefined);
+
+
